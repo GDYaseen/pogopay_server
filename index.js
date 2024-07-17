f327ea27c7affe92588b93184693053b7d7b6a58
Yassine Lahriga <54582121+GDYaseen@users.noreply.github.com>
2024-07-17 17:59:26 +0100

    update

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import express, { json } from "express"
 import https from "https"
 import fs from "fs"
 import { dbConnect } from "./db.js"
-import AuthRouter from "./routes/auth.js"
-import UserRouter from "./routes/user.js"
-import PaimentRouter from "./routes/paiment.js"
+import AuthRouter from "./routes/authRoute.js"
+import UserRouter from "./routes/userRoute.js"
+import PaimentRouter from "./routes/paimentRoute.js"
+import CarteRouter from "./routes/carteRoute.js"
 import cors from "cors"
 
 const app = express()
 const port = 3000
 
-app.use(cors({ origin: "http://localhost:5173" }))
+app.use(cors({ origin: "*" }))
 
 // https configuration
 // const privateKey = fs.readFileSync("localhost-key.pem", "utf8")
@@ -35,13 +36,14 @@ app.use(json())
 app.use("/auth", AuthRouter)
 app.use("/user", UserRouter)
 app.use("/paiment", PaimentRouter)
+app.use("/carte", CarteRouter)
 
 dbConnect()
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err))
 
 app.get("/", async (req, res) => {
-  res.send("hi")
+  res.send("hi, the server is active")
 })
 
 // const httpsServer = https.createServer(credentials, app)
---
+++ routes/carteRoute.js
@@ -0,0 +1,76 @@
+import axios from "axios"
+import { Router } from "express"
+import crypto from "crypto"
+const router = Router()
+import { config } from "dotenv"
+
+config()
+
+router.all("/add-card",async (req,res)=>{
+    const postData = {
+        clientid: process.env.CLIENTID,
+        amount: "1.00",
+        okUrl: process.env.OKURL,
+        failUrl: process.env.FAILURL,
+        TranType: "PreAuth",
+        callbackUrl: process.env.CALLBACKURL,
+        currency: "504",
+        rnd: getMicrosecondsSinceEpoch(),
+        storetype: "3DPAYHOSTING",
+        hashAlgorithm: "ver3",
+        lang: "fr",
+        encoding: "UTF-8",
+        MERCHANTSAFE: "MERCHANTSAFE",
+        MERCHANTSAFEKEY: "TOKEN123456", //rib
+        MERCHANTSAFEAUTHTYPE: "3DPAYAUTH",
+        // MERCHANTSAFEACQUIRER: process.env.MERCHANTSAFEACQUIRER,
+        MERCHANTGROUPID: process.env.MERCHANTGROUPID
+      };
+    
+    const storeKey = process.env.STOREKEY; // Set by the merchant or delivered by the CMI Integration team
+    const hash = createHashFromPostData(postData, storeKey);
+    postData.HASH = hash;
+
+    let formHtml = '<html><head><title>Redirecting...</title></head><body onload="document.forms[0].submit()">';
+    formHtml += '<form method="post" action="https://testpayment.cmi.co.ma/fim/est3Dgate">';
+
+    for (const [key, value] of Object.entries(postData)) {
+        formHtml += `<input type="hidden" name="${key}" value="${value}" />\n`;
+    }
+
+    formHtml += '</form></body></html>';
+    
+    res.send(formHtml);
+})
+
+
+function createHashFromPostData(postData, storeKey) {
+    const postParams = Object.keys(postData).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+    
+    let hashval = '';
+    postParams.forEach(param => {
+        const paramValue = postData[param].trim();
+        const escapedParamValue = paramValue.replace(/\\/g, '\\\\').replace(/\|/g, '\\|');
+        
+        const lowerParam = param.toLowerCase();
+        if (lowerParam !== 'hash' && lowerParam !== 'encoding') {
+            hashval += escapedParamValue + '|';
+        }
+        // console.log(lowerParam,":",escapedParamValue)
+    });
+
+    const escapedStoreKey = storeKey.replace(/\\/g, '\\\\').replace(/\|/g, '\\|');
+    hashval += escapedStoreKey;
+
+    const calculatedHashValue = crypto.createHash('sha512').update(hashval, 'utf8').digest('hex');
+    const hash = Buffer.from(calculatedHashValue, 'hex').toString('base64');
+    
+    return hash;
+}
+
+function getMicrosecondsSinceEpoch() {
+    const now = new Date();
+    return (now.getTime() * 1000 + now.getMilliseconds() * 1000).toString();
+  }
+
+export default router
